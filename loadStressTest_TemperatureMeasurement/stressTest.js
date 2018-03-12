f0b6060151bf29b5307ca9ed9892aa3ff5f06048
Marcel Petrick <mail@marcelpetrick.it>
2018-03-12 22:52:09 +0100

    stress test: improved the version; buttonA does not start/stop the computation; buttonB prints out current state; in case the temperpature changed the current one is as well displayed

--- loadStressTest_TemperatureMeasurement/stressTest.js
+++ loadStressTest_TemperatureMeasurement/stressTest.js
@@ -1,24 +1,36 @@
-let placeholder = 0
+let m_placeholder = 0
 let m_doComputation = 0
-// pressing a should start some heavy computation
+let m_currentTemperature = 0
+
+// pressing buttonB should stop the computation
+input.onButtonPressed(Button.B, () => {
+    basic.showNumber(m_currentTemperature)
+})
+
+// pressing buttonA should start some heavy computation
 // inside the event-loop (multiple division in a loop)
 input.onButtonPressed(Button.A, () => {
-    m_doComputation = 1
+    // is: invert the current value
+    m_doComputation = (m_doComputation + 1) % 1
 })
-// pressing b should stop that
-input.onButtonPressed(Button.B, () => {
-    m_doComputation = 0
-})
+
 m_doComputation = 0
 // display always the current temperature
 basic.forever(() => {
-    basic.showString(" ")
-    basic.showNumber(input.temperature())
+    // save the current temperature
+    let oldTemperature = m_currentTemperature;
+    m_currentTemperature = input.temperature();
+
+    // if changed from last run, then display it
+    if (oldTemperature != m_currentTemperature) {
+        basic.showNumber(m_currentTemperature)
+    }
+
+    // here in the main-loop is the heavy computation-block: has to be improved ... does not look really heavy
     if (m_doComputation) {
-        basic.showString("a")
-        for (let iterator = 0; iterator <= 200000; iterator++) {
-            placeholder = 43264257 / 7
+        for (let iterator = 0; iterator <= 999; iterator++) {
+            m_placeholder = 43264257 / 7
         }
-        basic.showString("b")
     }
 })
+
