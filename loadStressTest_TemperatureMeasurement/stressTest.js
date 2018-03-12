ecf336c13b0311730e6370862a48045399a5f931
Marcel Petrick <mail@marcelpetrick.it>
2018-03-12 17:51:12 +0100

    stress test: improved version, a-button for starting and b-button for stopping the heavy processing now. TODO find something more work intensive :)

--- loadStressTest_TemperatureMeasurement/stressTest.js
+++ loadStressTest_TemperatureMeasurement/stressTest.js
@@ -1,17 +1,24 @@
-let item = 0
-//pressing a should start some heavy computation (multiple division in a loop)
+let placeholder = 0
+let m_doComputation = 0
+// pressing a should start some heavy computation
+// inside the event-loop (multiple division in a loop)
 input.onButtonPressed(Button.A, () => {
-    basic.showString("a")
-    for (let index = 0; index <= 10000000; index++) {
-        item = 2048 / 7
-    }
-    basic.showString("b")
+    m_doComputation = 1
 })
-
+// pressing b should stop that
+input.onButtonPressed(Button.B, () => {
+    m_doComputation = 0
+})
+m_doComputation = 0
 // display always the current temperature
 basic.forever(() => {
     basic.showString(" ")
-    basic.pause(1000)
     basic.showNumber(input.temperature())
-    basic.pause(1000)
+    if (m_doComputation) {
+        basic.showString("a")
+        for (let iterator = 0; iterator <= 200000; iterator++) {
+            placeholder = 43264257 / 7
+        }
+        basic.showString("b")
+    }
 })
