4285a6d1ba4351ad5e8b580fb4cbb802f026d7f2
Marcel Petrick <mail@marcelpetrick.it>
2018-03-12 22:55:36 +0100

    stress test: fixed on/off bug; added some output about the current state

--- loadStressTest_TemperatureMeasurement/stressTest.js
+++ loadStressTest_TemperatureMeasurement/stressTest.js
@@ -1,34 +1,34 @@
 let m_placeholder = 0
-let m_doComputation = 0
 let m_currentTemperature = 0
-
+let oldTemperature = 0
+// pressing buttonA should start some heavy
+// computation inside the event-loop (multiple
+// division in a loop)
+input.onButtonPressed(Button.A, () => {
+    // is: invert the current value
+    m_doComputation = (m_doComputation + 1) % 2
+})
 // pressing buttonB should stop the computation
 input.onButtonPressed(Button.B, () => {
     basic.showNumber(m_currentTemperature)
+    basic.showString(m_doComputation ? "computing" : "idle")
 })
-
-// pressing buttonA should start some heavy computation
-// inside the event-loop (multiple division in a loop)
-input.onButtonPressed(Button.A, () => {
-    // is: invert the current value
-    m_doComputation = (m_doComputation + 1) % 1
-})
-
+let m_doComputation = 0
 m_doComputation = 0
 // display always the current temperature
 basic.forever(() => {
     // save the current temperature
-    let oldTemperature = m_currentTemperature;
-    m_currentTemperature = input.temperature();
-
+    oldTemperature = m_currentTemperature
+    m_currentTemperature = input.temperature()
     // if changed from last run, then display it
     if (oldTemperature != m_currentTemperature) {
         basic.showNumber(m_currentTemperature)
     }
-
-    // here in the main-loop is the heavy computation-block: has to be improved ... does not look really heavy
+    // here in the main-loop is the heavy
+    // computation-block: has to be improved ... does not
+    // look really heavy
     if (m_doComputation) {
-        for (let iterator = 0; iterator <= 999; iterator++) {
+        for (let iterator = 0; iterator <= 99999; iterator++) {
             m_placeholder = 43264257 / 7
         }
     }
