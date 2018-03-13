85882b985cc47d62eac4cf4d1aaab2fc88801da6
Marcel Petrick <mail@marcelpetrick.it>
2018-03-13 15:12:47 +0100

    stressTest: added a given, recursive fibonacci-function for the computing-part; cleaned the code

--- loadStressTest_TemperatureMeasurement/stressTest.js
+++ loadStressTest_TemperatureMeasurement/stressTest.js
@@ -1,7 +1,8 @@
-let list: number[] = []
-let m_placeholder = 0
 let m_currentTemperature = 0
+let m_placeholder = 0
 let oldTemperature = 0
+let m_doComputation = 0
+
 // pressing buttonA should start some heavy
 // computation inside the event-loop (multiple
 // division in a loop)
@@ -9,17 +10,13 @@ input.onButtonPressed(Button.A, () => {
     // is: invert the current value
     m_doComputation = (m_doComputation + 1) % 2
 })
+
 // pressing buttonB should stop the computation
 input.onButtonPressed(Button.B, () => {
     basic.showNumber(m_currentTemperature)
-    basic.showString(m_doComputation ? "run" : "idle")
+    basic.showString(m_doComputation ? "running" : "idle")
 })
-let m_doComputation = 0
-m_placeholder = 0
-m_doComputation = 0
-for (let i = 0; i < 100; i++) {
-    list.push(Math.random(10))
-}
+
 // display always the current temperature
 basic.forever(() => {
     // save the current temperature
@@ -30,14 +27,16 @@ basic.forever(() => {
         basic.showNumber(m_currentTemperature)
     }
 })
+
 control.inBackground(() => {
     // here in the main-loop is the heavy
     // computation-block: has to be improved ... does not
     // look really heavy
     if (m_doComputation) {
-        for (let value of list) {
-            value = value * Math.random(10) % Math.random(101)
-        }
+        fib(4096);
     }
 })
 
+function fib(value: number): number {
+    return value <= 1 ? value : fib(value - 1) + fib(value - 2);
+}
