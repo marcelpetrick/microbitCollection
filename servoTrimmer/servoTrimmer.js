efe82946eef1966426970a4261831eaaea36f7f8
Marcel Petrick <mail@marcelpetrick.it>
2018-03-19 22:35:48 +0100

    servoTrimmer: helper for the continuous rotation servos

---
+++ servoTrimmer/servoTrimmer.js
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, () => {
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 180)
+})
+input.onButtonPressed(Button.B, () => {
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P2, 0)
+})
+input.onButtonPressed(Button.AB, () => {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+})
+
