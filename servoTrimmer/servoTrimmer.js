c41713b8e4b7500f797566eac4bfe79dc1a0ad0d
Marcel Petrick <mail@marcelpetrick.it>
2018-03-19 23:05:43 +0100

    servoTrimmer: both in the same direction

--- servoTrimmer/servoTrimmer.js
+++ servoTrimmer/servoTrimmer.js
@@ -1,9 +1,9 @@
 input.onButtonPressed(Button.A, () => {
-    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 180)
 })
 input.onButtonPressed(Button.B, () => {
-    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 0)
 })
 input.onButtonPressed(Button.AB, () => {
